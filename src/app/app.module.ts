d4dd001f6ee0e14ba1d8d942a38337963207ceb2
manh3698 <manh3698@gmail.com>
2020-05-18 14:49:58 +0700

    commit by manh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
