c6d6e4ede2626f68fb09f604cd2ba25a749fbbef
NhanSirenas <31368540+sirenas23298@users.noreply.github.com>
2020-05-22 15:41:50 +0700

    ads
    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component'
 import { ListUserComponent } from './user/list-user/list-user.component'
 import { EditUserComponent } from './user/edit-user/edit-user.component'
+import { AddUserComponent } from './user/add-user/add-user.component'
 const routes: Routes = [
   { path: 'home',component: HomepageComponent },
   { path: '',component: LoginComponent },
   { path: 'user',component: ListUserComponent },
-  { path: 'edit-user', component: EditUserComponent}
+  { path: 'edit-user', component: EditUserComponent},
+  { path: 'add-user', component: AddUserComponent}
 ];
 
 @NgModule({
